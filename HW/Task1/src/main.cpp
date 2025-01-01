019a4fb3113016965b924532a101627b6df6be94
OG Michael <6el0zu61979@mail.ru>
2025-01-01 21:59:19 +0500

    Task1

--- HW/Task1/src/main.cpp
+++ HW/Task1/src/main.cpp
@@ -1,6 +1,82 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include <ctime>
+using namespace std;
 
-int main()
-{
-    
+struct Task {
+    string name;
+    time_t startTime;
+    time_t endTime;
+};
+
+map<string, Task> completedTasks;
+Task currentTask;
+bool isTaskRunning = false;
+
+void beginTask(string taskName) {
+    if (isTaskRunning) {
+        currentTask.endTime = time(nullptr);
+        completedTasks[currentTask.name] = currentTask;
+    }
+
+    currentTask.name = taskName;
+    currentTask.startTime = time(nullptr);
+    currentTask.endTime = 0;
+    isTaskRunning = true;
+
+    cout << "Task started: " << taskName << endl;
+}
+
+void endTask() {
+    if (isTaskRunning) {
+        currentTask.endTime = time(nullptr);
+        completedTasks[currentTask.name] = currentTask;
+        isTaskRunning = false;
+        cout << "Task completed: " << currentTask.name << endl;
+    } else {
+        cout << "No current task to complete." << endl;
+    }
+}
+
+void showStatus() {
+    cout << "Completed tasks:" << endl;
+    map<string, Task>::iterator it;
+    for (it = completedTasks.begin(); it != completedTasks.end(); ++it) {
+        Task task = it->second;
+        double duration = difftime(task.endTime, task.startTime) / 3600;
+        cout << "Task: " << task.name << ", Time: " << duration << " hours" << endl;
+    }
+
+    if (isTaskRunning) {
+        cout << "Current task: " << currentTask.name << endl;
+    } else {
+        cout << "No current task." << endl;
+    }
+}
+
+int main() {
+    string command;
+
+    while (true) {
+        cout << "Enter command (begin, end, status, exit): ";
+        getline(cin, command);
+
+        if (command == "begin") {
+            cout << "Enter tasks name: ";
+            string taskName;
+            getline(cin, taskName);
+            beginTask(taskName);
+        } else if (command == "end") {
+            endTask();
+        } else if (command == "status") {
+            showStatus();
+        } else if (command == "exit") {
+            break;
+        } else {
+            cout << "Unknown command." << endl;
+        }
+    }
+
+    return 0;
 }
